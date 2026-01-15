07a3fcdfc3491383b6f43caf5e7ef464db7ad241
Umesh Sahu <umeshkumar637825@gmail.com>
2026-01-15 15:31:11 +0530

    Update root route response with additional information

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("🚀 CI/CD pipeline working on Ubuntu!"));
-
-app.get("/", (req, res) => res.send("🚀 HELLO EVERYONE GOOD MORNING"));
+app.get("/", (req, res) => {
+  res.send(`
+    🚀 CI/CD pipeline working on Ubuntu! <br>
+    🚀 HELLO EVERYONE GOOD MORNING <br>
+    🚀 Eoxys IT Solution, Nami Nagar, Jaipur
+  `);
+});
 
 app.listen(3000, () => console.log("App running on port 3000"));
-
